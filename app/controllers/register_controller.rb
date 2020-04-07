780d46ddee86ee0e04ab29b0eea12a2b81d476e0
Tyler Porter <tylerpporter@gmail.com>
2020-04-06 18:41:57 -0600

    Refactor create method for RegisterController

--- app/controllers/register_controller.rb
+++ app/controllers/register_controller.rb
@@ -5,15 +5,7 @@ class RegisterController < ApplicationController
 
   def create
     user = User.create(user_params)
-    if user.save
-      flash[:notice] = "Welcome #{params[:name]} Registration Successful"
-      session[:user_id] = user.id
-      redirect_to "/profile"
-    else
-      flash[:error] = user.errors.full_messages.to_sentence
-      @user_info = user_params
-      render :new
-    end
+    user.save ? success(user) : failure(user)
   end
 
   private
@@ -21,4 +13,17 @@ class RegisterController < ApplicationController
   def user_params
     params.permit(:name, :address, :city, :state, :zip, :email, :password, :password_confirmation)
   end
+
+  def success(user)
+    flash[:notice] = "Welcome #{params[:name]} Registration Successful"
+    session[:user_id] = user.id
+    redirect_to "/profile"
+  end
+
+  def failure(user)
+    flash[:error] = user.errors.full_messages.to_sentence
+    @user_info = user_params
+    render :new
+  end
+
 end
