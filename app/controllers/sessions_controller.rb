ff3012284cbd4d07abd569476bf474bad0c70985
Anthony Gomes <code@tonygom.es>
2020-04-07 15:59:42 -0600

    Add create action for sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,13 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:success] = "You are logged in"
+      redirect_to "/profile"
+    end
+  end
 end
