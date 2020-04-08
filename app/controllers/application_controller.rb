ba7b8fffd5aa0f39a4bc614b84edb50441d68e26
Ethan Hocking <eshocking253@gmail.com>
2020-04-07 19:26:49 -0600

    Add current user access and authorization methods to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,15 @@ class ApplicationController < ActionController::Base
     @cart ||= Cart.new(session[:cart] ||= Hash.new(0))
   end
 
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def current_admin?
+    current_user && current_user.admin?
+  end
+
+  def current_merchant?
+    current_user && current_user.merchant?
+  end
 end
