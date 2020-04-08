8befff29fe7f68058129bb4f79328efc7b8793d6
Tyler Porter <tylerpporter@gmail.com>
2020-04-08 16:41:22 -0600

    Refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,39 +1,30 @@
 Rails.application.routes.draw do
   #welcome
   root 'welcome#index'
+
   #merchants
-  get '/merchants', to: 'merchants#index'
-  get '/merchants/new', to: 'merchants#new'
-  get '/merchants/:id', to: 'merchants#show'
-  post '/merchants', to: 'merchants#create'
-  get '/merchants/:id/edit', to: 'merchants#edit'
-  patch '/merchants/:id', to: 'merchants#update'
-  delete '/merchants/:id', to: 'merchants#destroy'
+  resources :merchants do
+    #merchant items
+    resources :items, only: [:index, :new, :create]
+  end
+
   #items
-  get '/items', to: 'items#index'
-  get '/items/:id', to: 'items#show'
-  get '/items/:id/edit', to: 'items#edit'
-  patch '/items/:id', to: 'items#update'
-  #merchant_items
-  get '/merchants/:merchant_id/items', to: 'items#index'
-  get '/merchants/:merchant_id/items/new', to: 'items#new'
-  post '/merchants/:merchant_id/items', to: 'items#create'
-  delete '/items/:id', to: 'items#destroy'
+  resources :items, except: [:new, :create] do
+      #item_reviews
+      resources :reviews, only: [:new, :create]
+  end
+
   #item_reviews
-  get '/items/:item_id/reviews/new', to: 'reviews#new'
-  post '/items/:item_id/reviews', to: 'reviews#create'
-  get '/reviews/:id/edit', to: 'reviews#edit'
-  patch '/reviews/:id', to: 'reviews#update'
-  delete '/reviews/:id', to: 'reviews#destroy'
+  resources :reviews, only: [:edit, :update, :destroy]
+
   #cart
   post '/cart/:item_id', to: 'cart#add_item'
   get '/cart', to: 'cart#show'
   delete '/cart', to: 'cart#empty'
   delete '/cart/:item_id', to: 'cart#remove_item'
+
   #orders
-  get '/orders/new', to: 'orders#new'
-  post '/orders', to: 'orders#create'
-  get '/orders/:id', to: 'orders#show'
+  resources :orders, only: [:new, :create, :show]
 
   #register
   get '/register', to: 'register#new'
@@ -54,8 +45,10 @@ Rails.application.routes.draw do
     get '/', to: "dashboard#show"
     get '/users', to: 'users#index'
   end
+
   #merchant_user
   namespace :merchant do
     get '/', to: "dashboard#show"
   end
+
 end
