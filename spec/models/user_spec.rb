090edcde8cc87cfc339a2f00f299153c1691a37a
Anthony Gomes <code@tonygom.es>
2020-04-08 08:11:29 -0600

    Add basic user model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,4 +12,21 @@ RSpec.describe User do
     it { should validate_presence_of :password }
     it { should validate_presence_of :password_confirmation }
   end
+
+  describe User do
+      it "can be created as a basic user" do
+        user = User.create(name: "basic_test_user",
+                           address: "1163 S Dudley St",
+                           city: "Lakewood",
+                           state: "CO",
+                           zip: "80232",
+                           email: "campryan@comcast.net",
+                           password: "password",
+                           password_confirmation: "password",
+                           role: 0)
+
+        expect(user.role).to eq("basic")
+        expect(user.basic?).to be_truthy
+      end
+    end
 end
