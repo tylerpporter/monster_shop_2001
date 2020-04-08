f2a002c7f7b840dda3e4ffa5d1d7b168c2fe3050
Ryan Camp <campryan@comcast.net>
2020-04-07 18:29:47 -0600

    migrate role attribute for user, and expand navbar links for visitors

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  #welcome
+  root 'welcome#index'
+  #merchants
   get '/merchants', to: 'merchants#index'
   get '/merchants/new', to: 'merchants#new'
   get '/merchants/:id', to: 'merchants#show'
@@ -8,29 +9,28 @@ Rails.application.routes.draw do
   get '/merchants/:id/edit', to: 'merchants#edit'
   patch '/merchants/:id', to: 'merchants#update'
   delete '/merchants/:id', to: 'merchants#destroy'
-
+  #items
   get '/items', to: 'items#index'
   get '/items/:id', to: 'items#show'
   get '/items/:id/edit', to: 'items#edit'
   patch '/items/:id', to: 'items#update'
-
+  #merchant_items
   get '/merchants/:merchant_id/items', to: 'items#index'
   get '/merchants/:merchant_id/items/new', to: 'items#new'
   post '/merchants/:merchant_id/items', to: 'items#create'
   delete '/items/:id', to: 'items#destroy'
-
+  #item_reviews
   get '/items/:item_id/reviews/new', to: 'reviews#new'
   post '/items/:item_id/reviews', to: 'reviews#create'
-
   get '/reviews/:id/edit', to: 'reviews#edit'
   patch '/reviews/:id', to: 'reviews#update'
   delete '/reviews/:id', to: 'reviews#destroy'
-
+  #cart
   post '/cart/:item_id', to: 'cart#add_item'
   get '/cart', to: 'cart#show'
   delete '/cart', to: 'cart#empty'
   delete '/cart/:item_id', to: 'cart#remove_item'
-
+  #orders
   get '/orders/new', to: 'orders#new'
   post '/orders', to: 'orders#create'
   get '/orders/:id', to: 'orders#show'
@@ -39,4 +39,6 @@ Rails.application.routes.draw do
   post '/register', to: 'register#create'
   #profile
   get '/profile', to: 'profile#show'
+  #login
+  get '/login', to: 'login#new'
 end
--- spec/features/navigation_spec.rb
+++ spec/features/navigation_spec.rb
@@ -3,9 +3,10 @@ require 'rails_helper'
 
 RSpec.describe 'Site Navigation' do
   describe 'As a Visitor' do
+    before(:each) do
+      visit "/"
+    end
     it "I see a nav bar with links to all pages" do
-      visit '/merchants'
-
       within 'nav' do
         click_link 'All Items'
       end
@@ -17,21 +18,30 @@ RSpec.describe 'Site Navigation' do
       end
 
       expect(current_path).to eq('/merchants')
-    end
-
-    it "I can see a cart indicator on all pages" do
-      visit '/merchants'
 
       within 'nav' do
-        expect(page).to have_content("Cart: 0")
+        click_link("Cart: 0")
       end
 
-      visit '/items'
+      expect(current_path).to eq('/cart')
 
       within 'nav' do
-        expect(page).to have_content("Cart: 0")
+        click_link("Login")
       end
 
+      expect(current_path).to eq('/login')
+
+      within 'nav' do
+        click_link("Register")
+      end
+
+      expect(current_path).to eq('/register')
+
+      within('nav') do
+        click_link("Home")
+      end
+
+      expect(current_path).to eq('/')
     end
   end
 end
