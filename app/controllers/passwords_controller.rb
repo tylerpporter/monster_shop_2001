df22fe845febe3fa950191ab23c1c8f4d5026e0c
Anthony Gomes <code@tonygom.es>
2020-04-09 14:45:41 -0600

    Update passwords controller

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -3,20 +3,20 @@ class PasswordsController < ApplicationController
   end
 
   def update
-    user = User.find_by(email: params[:email])
-    user.update(user_params)
-    if user.save
-      flash[:success] = "Your password is updated"
+    @user = current_user
+    @user.update(user_params)
+    if @user.save
+      flash[:success] = "Your Password Has Been Updated"
       redirect_to "/profile"
     else
       flash[:error] = "Please enter valid & matching passwords"
-      render :update
+      redirect_to "/password/edit"
     end
   end
 
   private
 
   def user_params
-    params.require(:current_user).permit(:password, :password_confirmation)
+    params.permit(:password, :password_confirmation)
   end
 end
