31fdbf7fd086725f0f7760d86f6b3ed8d533ac54
Ryan Camp <campryan@comcast.net>
2020-04-12 15:57:46 -0600

    Create items routes for merchant namespace

--- config/routes.rb
+++ config/routes.rb
@@ -59,5 +59,6 @@ Rails.application.routes.draw do
   namespace :merchant do
     get '/', to: 'dashboard#show'
     get '/orders/:id', to: 'orders#show'
+    get '/items', to: 'items#index'
   end
 end
