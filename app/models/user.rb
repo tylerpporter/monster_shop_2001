d7618aa99fae2ba4a75f4ffcb5992b9c77846eeb
Anthony Gomes <code@tonygom.es>
2020-04-11 18:06:29 -0600

    Add has_one relationship w/ merchants through merchant_employee

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ApplicationRecord
 
   enum role: { regular: 0, merchant: 1, admin: 2 }
 
+  has_one :merchant_employee
+  has_one :merchant, through: :merchant_employee
   has_secure_password
 end
