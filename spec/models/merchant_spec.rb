bf605c33df42169f15a5e742c3490fb0b52d6901
Anthony Gomes <code@tonygom.es>
2020-04-12 11:14:20 -0600

    Add #pending_orders for Merchant model spec

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -75,5 +75,22 @@ describe Merchant, type: :model do
       expect(MerchantEmployee.last.merchant_id).to eq(@meg.id)
       expect(MerchantEmployee.last.user_id).to eq(@user.id)
     end
+
+    it 'can return merchant pending orders' do
+      twix = Merchant.create(name: "Twix's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+      chain = twix.items.create(name: "Chain", description: "It'll never break!", price: 40, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 22)
+      mchain = @meg.items.create(name: "MChain", description: "It'll never break!", price: 40, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 22)
+
+      order1 = @user.orders.create!(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033)
+      order2 = @user.orders.create!(name: 'Brian', address: '123 Brian Ave', city: 'Denver', state: 'CO', zip: 17033)
+      order3 = @user.orders.create!(name: 'Dao', address: '123 Mike Ave', city: 'Denver', state: 'CO', zip: 17033)
+
+      order1.item_orders.create!(item: @tire, price: @tire.price, quantity: 1)
+      order2.item_orders.create!(item: chain, price: chain.price, quantity: 2)
+      order3.item_orders.create!(item: mchain, price: mchain.price, quantity: 3)
+      order3.item_orders.create!(item: @tire, price: @tire.price, quantity: 2)
+
+      expect(@meg.pending_orders).to eq([order1, order3])
+    end
   end
 end
