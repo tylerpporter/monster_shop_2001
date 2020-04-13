a4540d45ef2425ef0208b7595a5f5d2a98936e11
Ethan Hocking <eshocking253@gmail.com>
2020-04-13 16:15:54 -0600

    Added class method test for .gather

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -15,7 +15,7 @@ describe Order, type: :model do
     it {should belong_to :user}
   end
 
-  describe 'instance methods' do
+  context "methods" do
     before :each do
       @user = User.create(name: "regular_test_user",
                          address: "1163 S Dudley St",
@@ -32,29 +32,50 @@ describe Order, type: :model do
       @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
       @pull_toy = @brian.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
 
-      @order_1 = @user.orders.create!(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033)
+      @order_1 = @user.orders.create!(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033, status: 0)
 
       @order_1.item_orders.create!(item: @tire, price: @tire.price, quantity: 2)
       @order_1.item_orders.create!(item: @pull_toy, price: @pull_toy.price, quantity: 3)
-    end
-    it 'grandtotal' do
-      expect(@order_1.grandtotal).to eq(230)
+
+      @order_2 = @user.orders.create!(name: 'Meg', address: '123 Stang St', city: 'Hershey', state: 'PA', zip: 80218, status: 1)
+      @item_order3 = @order_2.item_orders.create!(item: @tire, price: @tire.price, quantity: 4)
+      @item_order4 = @order_2.item_orders.create!(item: @pull_toy, price: @pull_toy.price, quantity: 2)
+
+      @order_3 = @user.orders.create!(name: 'Meg', address: '123 Stang St', city: 'Hershey', state: 'PA', zip: 80218, status: 2)
+      @item_order5 = @order_3.item_orders.create!(item: @tire, price: @tire.price, quantity: 4)
+
+      @order_4 = @user.orders.create!(name: 'Meg', address: '123 Stang St', city: 'Hershey', state: 'PA', zip: 80218, status: 3)
+      @item_order6 = @order_4.item_orders.create!(item: @tire, price: @tire.price, quantity: 4)
     end
 
-    it 'create_item_orders' do
-      item1 = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
-      item2 = @brian.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
-      @order_1.create_item_orders({item1 => 2, item2 => 1})
-      expect(@order_1.item_orders.count).to eq(4)
-      expect(@order_1.item_orders[0].class).to eq(ItemOrder)
+    describe 'instance methods' do
+      it '#grandtotal' do
+        expect(@order_1.grandtotal).to eq(230)
+      end
+
+      it '#create_item_orders' do
+        item1 = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+        pull_toy = @brian.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
+        @order_1.create_item_orders({item1 => 2, pull_toy => 1})
+        expect(@order_1.item_orders.count).to eq(4)
+        expect(@order_1.item_orders[0].class).to eq(ItemOrder)
+      end
+
+      it '#total_item_quantity' do
+        item1 = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+        pull_toy = @brian.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
+        @order_1.create_item_orders({item1 => 2, pull_toy => 1})
+        expect(@order_1.total_item_quantity).to eq(8)
+      end
     end
 
-    it 'total_item_quantity' do
-      item1 = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
-      item2 = @brian.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
-      @order_1.create_item_orders({item1 => 2, item2 => 1})
-      expect(@order_1.total_item_quantity).to eq(8)
+    describe "class methods" do
+      it '.gather' do
+        expect(Order.gather(:packaged)).to eq([@order_2])
+        expect(Order.gather(:pending)).to eq([@order_1])
+        expect(Order.gather(:shipped)).to eq([@order_3])
+        expect(Order.gather(:cancelled)).to eq([@order_4])
+      end
     end
-
   end
 end
