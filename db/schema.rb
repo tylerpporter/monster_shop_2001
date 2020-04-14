c9f1451084ba786482b568cc6098593acf459e90
Ryan Camp <campryan@comcast.net>
2020-04-14 13:00:53 -0600

    Add timestamps to User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200414133839) do
+ActiveRecord::Schema.define(version: 20200414185107) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -88,6 +88,8 @@ ActiveRecord::Schema.define(version: 20200414133839) do
     t.string "email"
     t.string "password_digest"
     t.integer "role", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "item_orders", "items"
