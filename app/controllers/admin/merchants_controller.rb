ae47c7888d4a4a7ec43ada026b4afe22ac960ca5
Ryan Camp <campryan@comcast.net>
2020-04-14 11:08:20 -0600

    Refactor update actin of admin/merchants controller to use ternary

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -9,14 +9,20 @@ class Admin::MerchantsController < Admin::BaseController
 
   def update
     merchant = Merchant.find(params[:id])
-    if merchant.enabled?
-      merchant.disable
-      flash[:notice] = "#{merchant.name} is now disabled"
-    else
-      merchant.enable
-      flash[:notice] = "#{merchant.name} is now enabled"
-    end
+    merchant.enabled? ?  disable(merchant) : enable(merchant)
     redirect_to admin_merchants_path
+  end
+
+  private
+
+  def enable(merchant)
+    merchant.enable
+    flash[:notice] = "#{merchant.name} is now enabled"
+  end
+
+  def disable(merchant)
+    merchant.disable
+    flash[:notice] = "#{merchant.name} is now disabled"
   end
 
 end
