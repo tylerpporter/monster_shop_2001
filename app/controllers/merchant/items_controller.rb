5709cab17e398d1f765b7dcb4fa5ac9dae8d0ae2
Tyler Porter <tylerpporter@gmail.com>
2020-04-13 21:52:44 -0600

    Create #flash_notice helper for Merchant::ItemsController

--- app/controllers/merchant/items_controller.rb
+++ app/controllers/merchant/items_controller.rb
@@ -7,8 +7,7 @@ class Merchant::ItemsController < Merchant::BaseController
   def update
     item = Item.find(params[:id])
     item.update(update_params)
-    flash[:notice] = "#{item.name} is no longer for sale" if !item.active?
-    flash[:notice] = "#{item.name} is now available for sale" if item.active?
+    flash_notice(item)
     redirect_to '/merchant/items'
   end
 
@@ -16,6 +15,11 @@ class Merchant::ItemsController < Merchant::BaseController
 
   def update_params
     params.permit(:active?)
+  end
+
+  def flash_notice(item)
+    flash[:notice] = "#{item.name} is no longer for sale" if !item.active?
+    flash[:notice] = "#{item.name} is now available for sale" if item.active?
   end
 
 end
